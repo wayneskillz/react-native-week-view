9322d0aa2068b5166efc0441b49e9c95e28c4220
George Alonge <georgedelson@gmail.com>
2020-01-10 23:21:49 +0100

    update fontsize and display style for 3 day view

--- src/Event/Event.styles.js
+++ src/Event/Event.styles.js
@@ -1,19 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   item: {
-    alignItems: 'center',
-    position: 'absolute',
+    alignItems: "center",
+    position: "absolute",
     paddingVertical: 8,
     paddingHorizontal: 2,
-    borderRadius: 0,
-    flex: 1,
+    borderRadius: 5,
+    flex: 1
   },
   description: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 17,
-  },
+    color: "#fff",
+    textAlign: "left",
+    fontSize: 14,
+    fontWeight: "bold",
+    fontFamily: "SFProDisplay-Bold",
+    padding: 5
+  }
 });
 
 export default styles;
